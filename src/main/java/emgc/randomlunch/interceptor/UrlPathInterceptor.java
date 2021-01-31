c110bec93f7e0b18610349fed1ca7dc64654711c
limheonjun <jumong40@naver.com>
2021-01-31 19:50:50 +0900

    URL PATH 인터셉터 추가

---
+++ src/main/java/emgc/randomlunch/interceptor/UrlPathInterceptor.java
@@ -0,0 +1,20 @@
+package emgc.randomlunch.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.AsyncHandlerInterceptor;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UrlPathInterceptor implements AsyncHandlerInterceptor {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info("URL PATH : " + request.getRequestURI());
+        return true;
+    }
+}
