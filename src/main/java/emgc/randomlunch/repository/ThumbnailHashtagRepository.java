d24cf917f72d25d7c0a22b55179cad0cf9a037b7
limheonjun <jumong40@naver.com>
2021-02-11 20:53:35 +0900

    HashtagRepository, ThumbnailHashtagRepository 추가

---
+++ src/main/java/emgc/randomlunch/repository/ThumbnailHashtagRepository.java
@@ -0,0 +1,7 @@
+package emgc.randomlunch.repository;
+
+import emgc.randomlunch.entity.ThumbnailHashtag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ThumbnailHashtagRepository extends JpaRepository<ThumbnailHashtag, Long> {
+}
