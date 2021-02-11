dd470ad26c10f58b445b17d32d5b1311b5a6e728
limheonjun <jumong40@naver.com>
2021-02-11 20:57:15 +0900

    ThumbnailListDto 수정

--- src/main/java/emgc/randomlunch/dto/ThumbnailListDto.java
+++ src/main/java/emgc/randomlunch/dto/ThumbnailListDto.java
@@ -1,5 +1,6 @@
 package emgc.randomlunch.dto;
 
+import emgc.randomlunch.entity.Thumbnail;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,8 +10,19 @@ public class ThumbnailListDto {
 
     private Long thumbnailId;
     private Long restaurantId;
-    private String path;
-    private String hashtag;
-    private String name;
+    private String thumbnailPath;
+    private Long thumbnailSize;
+    private int thumbnailHeight;
+    private int thumbnailWidth;
+
+    public ThumbnailListDto(Thumbnail thumbnail) {
+        thumbnailId = thumbnail.getId();
+        restaurantId = thumbnail.getRestaurant().getId();
+        thumbnailPath = thumbnail.getPath();
+        thumbnailSize = thumbnail.getSize();
+        thumbnailHeight = thumbnail.getThumbnailHeight();
+        thumbnailWidth = thumbnail.getThumbnailWidth();
+
+    }
 
 }
