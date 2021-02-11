1879f0ff34f492884b92856fd6148cbf4d313950
limheonjun <jumong40@naver.com>
2021-02-11 20:56:50 +0900

    RestaurantApi "/upload" 수정 및 "/list" 추가

--- src/main/java/emgc/randomlunch/api/RestaurantApi.java
+++ src/main/java/emgc/randomlunch/api/RestaurantApi.java
@@ -1,9 +1,11 @@
 package emgc.randomlunch.api;
 
+import emgc.randomlunch.dto.RestaurantListDto;
 import emgc.randomlunch.entity.Restaurant;
 import emgc.randomlunch.entity.Thumbnail;
 import emgc.randomlunch.repository.RestaurantRepository;
 import emgc.randomlunch.repository.ThumbnailRepository;
+import emgc.randomlunch.service.business.RestaurantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -24,34 +27,17 @@ public class RestaurantApi {
 
     private final RestaurantRepository restaurantRepository;
     private final ThumbnailRepository thumbnailRepository;
+    private final RestaurantService restaurantService;
 
     //    https://stackoverflow.com/questions/21329426/spring-mvc-multipart-request-with-json
     @PostMapping("/upload")
-    public void uploadRestaurant(MultipartFile file, @RequestPart("restaurant")Restaurant restaurant) {
+    public void uploadRestaurant(/*MultipartFile files[], */@RequestPart("restaurant")Restaurant restaurant) throws IOException {
         System.out.println("address : " + restaurant.getAddress());
         System.out.println("name : " + restaurant.getName());
-        System.out.println("file : " + file.getOriginalFilename());
-        //restaurantRepository.save(restaurant);
-        // 파일 이름 변경
-        UUID uuid = UUID.randomUUID();
-        String saveName = uuid + "_" + LocalDate.now() + "." + file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")+1);
+    }
 
-
-        // 저장할 File 객체를 생성(껍데기 파일)
-        File saveFile = new File(path,saveName); // 저장할 폴더 이름, 저장할 파일 이름
-
-        try {
-            file.transferTo(saveFile); // 업로드 파일에 saveFile이라는 껍데기 입힘
-            restaurantRepository.save(restaurant);
-            thumbnailRepository.save(Thumbnail.builder()
-                                        .id(null)
-                                        .path(path+saveName)
-                                        .size((int)saveFile.length())
-                                        .restaurant(restaurant)
-                                        .build()
-            );
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    @GetMapping("/list")
+    public List<RestaurantListDto> getAllRestaurants() {
+        return restaurantService.getAllRestaurnats();
     }
 }
