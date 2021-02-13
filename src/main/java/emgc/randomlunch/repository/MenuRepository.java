323b74335e8e82dfe8538028c391c2ff56f819f0
limheonjun <jumong40@naver.com>
2021-02-13 11:52:36 +0900

    가격 오름차순 정렬 findAll 추가

--- src/main/java/emgc/randomlunch/repository/MenuRepository.java
+++ src/main/java/emgc/randomlunch/repository/MenuRepository.java
@@ -1,7 +1,13 @@
 package emgc.randomlunch.repository;
 
 import emgc.randomlunch.entity.Menu;
+import emgc.randomlunch.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+
+    List<Menu> findByRestaurantOrderByPriceAsc(Restaurant restaurant);
+
 }
