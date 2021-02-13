7321c7af1aeb2015f032b4cbfdf007fdc1a21a6e
limheonjun <jumong40@naver.com>
2021-02-13 11:50:31 +0900

    lombok 애노테이션 추가

--- src/main/java/emgc/randomlunch/entity/ThumbnailHashtag.java
+++ src/main/java/emgc/randomlunch/entity/ThumbnailHashtag.java
@@ -1,13 +1,17 @@
 package emgc.randomlunch.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ThumbnailHashtag {
 
     @Id
