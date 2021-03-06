b16c1bf6bc6ef327bd6747bed834ed87b2037260
limheonjun <jumong40@naver.com>
2021-03-06 11:06:47 +0900

    RestaurantRepository 카테고리로 검색 추가

--- src/main/java/emgc/randomlunch/repository/RestaurantRepository.java
+++ src/main/java/emgc/randomlunch/repository/RestaurantRepository.java
@@ -1,7 +1,12 @@
 package emgc.randomlunch.repository;
 
+import emgc.randomlunch.entity.Category;
 import emgc.randomlunch.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+    List<Restaurant> findByCategory(Category category);
 }
