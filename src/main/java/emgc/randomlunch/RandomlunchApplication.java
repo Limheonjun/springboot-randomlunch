09c67a819618aeccf439ac1382078a850c27dff7
limheonjun <jumong40@naver.com>
2021-03-06 11:04:49 +0900

    HTTP연결을 HTTPS로 Redirection설정 추가

--- src/main/java/emgc/randomlunch/RandomlunchApplication.java
+++ src/main/java/emgc/randomlunch/RandomlunchApplication.java
@@ -1,13 +1,48 @@
 package emgc.randomlunch;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class RandomlunchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RandomlunchApplication.class, args);
+    }
+
+    @Bean
+    public ServletWebServerFactory serveltContainer(){
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(){
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection  collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    // http요청을 https인 5069포트로 리다이렉션
+    private Connector createStandardConnector(){
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setSecure(false);
+        connector.setPort(7647);
+        connector.setRedirectPort(5069);
+        return connector;
     }
 
 }
