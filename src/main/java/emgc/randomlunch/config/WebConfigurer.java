037bc49dae75f975bc8c5f872b8b21c8e91797cd
limheonjun <jumong40@naver.com>
2021-03-06 11:08:32 +0900

    WebConfigurer allowOrigins 추가

--- src/main/java/emgc/randomlunch/config/WebConfigurer.java
+++ src/main/java/emgc/randomlunch/config/WebConfigurer.java
@@ -1,7 +1,10 @@
 package emgc.randomlunch.config;
 
+import emgc.randomlunch.filter.ResponseFilter;
 import emgc.randomlunch.interceptor.UrlPathInterceptor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -22,9 +25,14 @@ public class WebConfigurer implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowCredentials(true) //JSESSIONID를 기반으로 인증
+                .allowCredentials(true).maxAge(3600) //JSESSIONID를 기반으로 인증
                 .allowedMethods("*")//기본 허용 메소드는 post와 get
-                .allowedOrigins("http://localhost:3000");
+                //끝부분에 /붙이면 안됨
+                .allowedOrigins("http://192.168.1.43:3000",  // 동일한 와이파이, 다른 기기에서 front서버 접속할 경우
+                        "http://dlagjswns.asuscomm.com:3000", // 외부 인터넷, 다른 기기에서 front서버 접속할 경우
+                        "http://dlagjswns.asuscomm.com:7647", // http접속 후 https로 리다이렉션하는 경우(작동 안됨)
+                        "https://react-randomlunch.vercel.app",
+                        "http://localhost:3000"); // 동일한 와이파이, 같은 데스크탑 내 front서버에서 접속할 경우
     }
 
     @Override
@@ -32,4 +40,13 @@ public class WebConfigurer implements WebMvcConfigurer {
         registry.addResourceHandler("/image/**")
                 .addResourceLocations("file:///"+path);
     }
+
+//    // responsefilter 적용 테스트
+//    @Bean
+//    public FilterRegistrationBean setFilterRegistration() {
+//        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new ResponseFilter());
+//        // filterRegistrationBean.setUrlPatterns(Collections.singletonList("/filtered/*")); // list 를 받는 메소드
+////        filterRegistrationBean.addUrlPatterns("/filtered/*"); // string 여러개를 가변인자로 받는 메소드
+//        return filterRegistrationBean;
+//    }
 }
