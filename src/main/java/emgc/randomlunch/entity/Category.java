d43c63de87f623ceb981e1128e7f123ff51fb79f
limheonjun <jumong40@naver.com>
2021-03-06 11:08:54 +0900

    Category 엔티티 추가

---
+++ src/main/java/emgc/randomlunch/entity/Category.java
@@ -0,0 +1,26 @@
+package emgc.randomlunch.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Restaurant> restaurant = new ArrayList<>();
+}
