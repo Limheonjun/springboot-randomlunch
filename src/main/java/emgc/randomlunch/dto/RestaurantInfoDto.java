9bc0cdef724c97ed9fcbc532cd81173a01773879
limheonjun <jumong40@naver.com>
2021-03-06 11:06:21 +0900

    RestaurantInfoDto 멤버 및 랜덤해시태그 추가

--- src/main/java/emgc/randomlunch/dto/RestaurantInfoDto.java
+++ src/main/java/emgc/randomlunch/dto/RestaurantInfoDto.java
@@ -1,13 +1,21 @@
 package emgc.randomlunch.dto;
 
+import emgc.randomlunch.entity.Restaurant;
+import emgc.randomlunch.entity.Thumbnail;
+import emgc.randomlunch.entity.ThumbnailHashtag;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class RestaurantInfoDto {
 
     private Long restaurantId;
@@ -16,6 +24,52 @@ public class RestaurantInfoDto {
     private String name;
     private String locationX;
     private String locationY;
+    private LocalTime openTime;
+    private LocalTime closeTime;
     private String address;
+    private Long categoryId;
+    private String fileName;
+    private List<String> hashtags = new ArrayList<>();
 
+    public void setOpenTime(String openTime) {
+//        this.openTime = LocalTime.of(openTime).format(DateTimeFormatter.ofPattern("HH:mm"));;
+        this.openTime = LocalTime.parse(openTime);
+    }
+
+    public void setCloseTime(String closeTime) {
+//        this.openTime = LocalTime.of(openTime).format(DateTimeFormatter.ofPattern("HH:mm"));;
+        this.closeTime = LocalTime.parse(closeTime);
+    }
+
+    public RestaurantInfoDto(Restaurant restaurant) {
+        this.restaurantId = restaurant.getId();
+        this.name = restaurant.getName();
+        this.openTime = restaurant.getOpenTime();
+        this.closeTime = restaurant.getCloseTime();
+        this.address = restaurant.getAddress();
+        this.categoryId = restaurant.getCategory().getId();
+        this.fileName = restaurant.getFileName();
+        this.hashtags = getRandomHashtags(restaurant);
+    }
+
+    private List<String> getRandomHashtags(Restaurant restaurant) {
+        List<String> hashtags = new ArrayList<>();
+
+        List<Thumbnail> thumbnailList = restaurant.getThumbnailList();
+        if (thumbnailList != null) {
+            Collections.shuffle(thumbnailList);
+            Loop1:
+            for (Thumbnail thumbnail : thumbnailList) {
+                List<ThumbnailHashtag> thumbnailHashtagList = thumbnail.getThumbnailHashtagList();
+                Loop2:
+                for (ThumbnailHashtag thumbnailHashtag : thumbnailHashtagList) {
+                    String word = thumbnailHashtag.getHashtag().getWord();
+                    hashtags.add(word);
+                    if(hashtags.size() > 0) break Loop1;
+                }
+
+            }
+        }
+        return hashtags;
+    }
 }
