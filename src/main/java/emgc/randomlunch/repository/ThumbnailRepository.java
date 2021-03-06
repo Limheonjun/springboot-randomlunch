0292b524ba030fdfd4b8770e4d4142a6dc86bc51
limheonjun <jumong40@naver.com>
2021-03-06 11:07:56 +0900

    ThumbnailRepository 상위 8개 검색 추가

--- src/main/java/emgc/randomlunch/repository/ThumbnailRepository.java
+++ src/main/java/emgc/randomlunch/repository/ThumbnailRepository.java
@@ -1,7 +1,13 @@
 package emgc.randomlunch.repository;
 
+import emgc.randomlunch.entity.Restaurant;
 import emgc.randomlunch.entity.Thumbnail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ThumbnailRepository extends JpaRepository<Thumbnail, Long> {
+
+    List<Thumbnail> findFirst8ByRestaurant(Restaurant restaurant);
+
 }
