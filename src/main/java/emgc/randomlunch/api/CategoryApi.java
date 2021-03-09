a0caa826f38200eabdabdfde7180134409fe8876
limheonjun <jumong40@naver.com>
2021-03-09 21:52:49 +0900

    CategoryApi 메소드 서비스로 이동

--- src/main/java/emgc/randomlunch/api/CategoryApi.java
+++ src/main/java/emgc/randomlunch/api/CategoryApi.java
@@ -1,17 +1,12 @@
 package emgc.randomlunch.api;
 
 import emgc.randomlunch.dto.CategoryInfoDto;
-import emgc.randomlunch.dto.MenuInfoDto;
-import emgc.randomlunch.entity.Category;
-import emgc.randomlunch.entity.Menu;
-import emgc.randomlunch.entity.Restaurant;
 import emgc.randomlunch.repository.CategoryRepository;
+import emgc.randomlunch.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,36 +14,26 @@ import java.util.stream.Collectors;
 public class CategoryApi {
 
     private final CategoryRepository categoryRepository;
+    private final CategoryService service;
 
     @PostMapping("/upload")
-    public String uploadCategory(@RequestBody CategoryInfoDto categoryInfoDto) throws Exception {
-        System.out.println("name : " + categoryInfoDto.getName());
-        Category findCategory = categoryRepository.findByName(categoryInfoDto.getName());
-        if(findCategory == null) {
-            Category c = Category.builder().name(categoryInfoDto.getName()).build();
-            categoryRepository.save(c);
-            return "저장되었습니다";
-        }
-        throw new Exception("이미 존재하는 아이디입니다");
+    public void uploadCategory(@RequestBody CategoryInfoDto categoryInfoDto) throws Exception {
+        service.addCategory(categoryInfoDto);
     }
 
     @GetMapping("/list")
-    public List<CategoryInfoDto> getAllCategories() {
-        List<Category> categories = categoryRepository.findAll();
-        return categories.stream().map(CategoryInfoDto::new).collect(Collectors.toList());
+    public List<CategoryInfoDto> getCategoryList() {
+        return service.getCategoryList();
     }
 
     @PostMapping("/edit")
-    public void editCategory(@RequestBody CategoryInfoDto categoryInfoDto) {
-        Category category = categoryRepository.findById(categoryInfoDto.getCategoryId()).get();
-        category.setName(categoryInfoDto.getName());
-        categoryRepository.save(category);
+    public void editCategoryInfo(@RequestBody CategoryInfoDto categoryInfoDto) throws Exception {
+        service.editCategory(categoryInfoDto);
     }
 
     @DeleteMapping("/delete")
-    public void deleteCategory(@RequestParam Long categoryId) {
-        Category category = categoryRepository.findById(categoryId).get();
-        categoryRepository.delete(category);
+    public void deleteCategory(@RequestParam Long categoryId) throws Exception {
+        service.deleteCategory(categoryId);
     }
 
 }
