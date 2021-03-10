288b1410194745fd62c9ed961cace54f17298bec
limheonjun <jumong40@naver.com>
2021-03-10 20:49:43 +0900

    MenuApi 비즈니스 로직 분리

--- src/main/java/emgc/randomlunch/api/MenuApi.java
+++ src/main/java/emgc/randomlunch/api/MenuApi.java
@@ -3,70 +3,43 @@ package emgc.randomlunch.api;
 import emgc.randomlunch.dto.MenuInfoDto;
 import emgc.randomlunch.entity.Menu;
 import emgc.randomlunch.entity.Restaurant;
-import emgc.randomlunch.repository.MenuRepository;
-import emgc.randomlunch.repository.RestaurantRepository;
+import emgc.randomlunch.service.MenuService;
+import emgc.randomlunch.service.RestaurantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/menu")
 @RequiredArgsConstructor
 public class MenuApi {
 
-    private final MenuRepository menuRepository;
-    private final RestaurantRepository restaurantRepository;
+    private final RestaurantService restaurantService;
+    private final MenuService menuService;
 
     @PostMapping("/upload")
     public void uploadMenu(@RequestBody MenuInfoDto menuInfoDto) {
-
-        Restaurant findRestaurant = restaurantRepository.findById(menuInfoDto.getRestaurantId()).get();
-
-        Menu menu = Menu.builder()
-                    .name(menuInfoDto.getName())
-                    .price(menuInfoDto.getPrice())
-                    .restaurant(findRestaurant)
-                    .build();
-
-        menuRepository.save(menu);
+        Restaurant findRestaurant = restaurantService.getRestaurant(menuInfoDto.getRestaurantId());
+        Menu menu = menuInfoDto.makeMenu(findRestaurant);
+        menuService.addMenu(menu);
     }
 
     @GetMapping("/list")
     public List<MenuInfoDto> getAllMenus(@RequestParam Long restaurantId) {
-        System.out.println("restaurantId " + restaurantId);
-        Restaurant findRestaurant = restaurantRepository.findById(restaurantId).get();
-        List<Menu> menuList = menuRepository.findByRestaurantOrderByPriceAsc(findRestaurant);
-        List<MenuInfoDto> menuInfoDtoList = new ArrayList<>();
-        for(Menu menu : menuList) {
-            menuInfoDtoList.add(MenuInfoDto.builder()
-                    .menuId(menu.getId())
-                    .restaurantId(menu.getRestaurant().getId())
-                    .name(menu.getName())
-                    .price(menu.getPrice())
-                    .build());
-        }
-
-        return menuInfoDtoList;
+        Restaurant findRestaurant = restaurantService.getRestaurant(restaurantId);
+        List<MenuInfoDto> menuList = menuService.getMenuList(findRestaurant);
+        return menuList;
     }
 
     @PostMapping("/edit")
-    public MenuInfoDto editMenu(@RequestBody MenuInfoDto menuInfoDto) {
-        System.out.println("edit " + menuInfoDto.getMenuId());
-        Menu menu = menuRepository.findById(menuInfoDto.getMenuId()).get();
-        menu.setName(menuInfoDto.getName());
-        menu.setPrice(menuInfoDto.getPrice());
-        Menu save = menuRepository.save(menu);
-        return new MenuInfoDto(save);
+    public void editMenu(@RequestBody MenuInfoDto menuInfoDto) {
+        menuService.editMenu(menuInfoDto);
     }
 
     @DeleteMapping("/delete")
     public void deleteMenu(@RequestParam Long menuId) {
-        Menu findMenu = menuRepository.findById(menuId).get();
-        menuRepository.delete(findMenu);
-        System.out.println("menuId" + menuId);
+        menuService.deleteMenu(menuId);
     }
 
 
