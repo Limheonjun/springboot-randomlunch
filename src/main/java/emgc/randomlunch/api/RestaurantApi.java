683a7410a6df5a6b9a435aabac4c7c2e42f75759
limheonjun <jumong40@naver.com>
2021-03-11 21:25:35 +0900

    RestaurantApi 비즈니스 로직 분리

--- src/main/java/emgc/randomlunch/api/RestaurantApi.java
+++ src/main/java/emgc/randomlunch/api/RestaurantApi.java
@@ -1,16 +1,14 @@
 package emgc.randomlunch.api;
 
-import emgc.randomlunch.dto.CategoryInfoDto;
 import emgc.randomlunch.dto.RestaurantInfoDto;
-import emgc.randomlunch.dto.RestaurantListDto;
 import emgc.randomlunch.entity.Category;
-import emgc.randomlunch.entity.Menu;
 import emgc.randomlunch.entity.Restaurant;
-import emgc.randomlunch.entity.Thumbnail;
 import emgc.randomlunch.repository.CategoryRepository;
 import emgc.randomlunch.repository.RestaurantRepository;
 import emgc.randomlunch.repository.ThumbnailRepository;
-import emgc.randomlunch.service.business.RestaurantService;
+import emgc.randomlunch.service.CategoryService;
+import emgc.randomlunch.service.RestaurantService;
+import emgc.randomlunch.util.FileUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
@@ -24,7 +22,6 @@ import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -36,119 +33,39 @@ public class RestaurantApi {
     @Value("${file.thumbnail.path}")
     private String path;
 
-    private final RestaurantRepository restaurantRepository;
-    private final ThumbnailRepository thumbnailRepository;
+    private final FileUtil fileUtil;
+    private final CategoryService categoryService;
     private final RestaurantService restaurantService;
-    private final CategoryRepository categoryRepository;
 
     //    https://stackoverflow.com/questions/21329426/spring-mvc-multipart-request-with-json
     @PostMapping("/upload")
     public void uploadRestaurant(@RequestPart("files") MultipartFile files[], @RequestPart("restaurant") RestaurantInfoDto restaurantInfoDto) throws IOException {
-        System.out.println("address : " + restaurantInfoDto.getAddress());
-        System.out.println("name : " + restaurantInfoDto.getName());
-
-        String saveName = null;
-        if(files != null) {
-            for (MultipartFile file : files) {
-
-                BufferedImage image = ImageIO.read(file.getInputStream());
-                int width = image.getWidth();
-                int height = image.getHeight();
-
-                // 파일 이름 변경
-                UUID uuid = UUID.randomUUID();
-                saveName = uuid + "_" + LocalDate.now() + "." + file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".") + 1);
-                // 저장할 File 객체를 생성(껍데기 파일)
-                File saveFile = new File(path, saveName); // 저장할 폴더 이름, 저장할 파일 이름
-                file.transferTo(saveFile); // 업로드 파일에 saveFile이라는 껍데기 입힘
-
-            }
-        }
-
-        Category findCategory = categoryRepository.findById(restaurantInfoDto.getCategoryId()).get();
-
-        Restaurant restaurant = Restaurant.builder()
-                .address(restaurantInfoDto.getAddress())
-                .name(restaurantInfoDto.getName())
-                .openTime(restaurantInfoDto.getOpenTime())
-                .closeTime(restaurantInfoDto.getCloseTime())
-                .category(findCategory)
-                .fileName(saveName)
-                .build();
-
-
-        restaurantRepository.save(restaurant);
+        Category category = categoryService.getCategory(restaurantInfoDto.getCategoryId());
+        List<String> fileName = fileUtil.saveFile(files, path);
+        restaurantService.addRestaurant(restaurantInfoDto, category, fileName.get(0));
     }
 
 
     @GetMapping("/list")
     public List<RestaurantInfoDto> getAllRestaurants(HttpSession httpSession, HttpServletResponse response) {
-        List<Restaurant> all = restaurantRepository.findAll();
-        List<RestaurantInfoDto> result = all.stream().map(RestaurantInfoDto::new).collect(Collectors.toList());
-        return result;
+        return restaurantService.getRestaurantList();
     }
 
     @GetMapping("/{categoryId}")
-    public List<RestaurantInfoDto> getRestaourant(@PathVariable Long categoryId) {
-        System.out.println("categoryId : " + categoryId);
-        Category findCategory = categoryRepository.findById(categoryId).get();
-        List<Restaurant> restaurants = restaurantRepository.findByCategory(findCategory);
-        List<RestaurantInfoDto> restaurantListDtoList = restaurants
-                .stream()
-                .map(restaurant -> new RestaurantInfoDto(restaurant))
-                .collect(Collectors.toList());
-        return restaurantListDtoList;
+    public List<RestaurantInfoDto> getRestaurantListByCategory(@PathVariable Long categoryId) {
+        Category category = categoryService.getCategory(categoryId);
+        List<RestaurantInfoDto> restaurants = restaurantService.getRestaurantListByCategory(category);
+        return restaurants;
     }
 
     @PostMapping("/edit")
-    public void editRestaurant(@RequestPart("files") MultipartFile files[], @RequestPart("restaurant") RestaurantInfoDto restaurantInfoDto) throws IOException {
-
-        Restaurant findRestaurant = restaurantRepository.findById(restaurantInfoDto.getRestaurantId()).get();
-
-        String saveName = null;
-
-        if(files != null) {
-            for (MultipartFile file : files) {
-
-                BufferedImage image = ImageIO.read(file.getInputStream());
-                int width = image.getWidth();
-                int height = image.getHeight();
-
-                // 파일 이름 변경
-                UUID uuid = UUID.randomUUID();
-                saveName = uuid + "_" + LocalDate.now() + "." + file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".") + 1);
-                // 저장할 File 객체를 생성(껍데기 파일)
-                File saveFile = new File(path, saveName); // 저장할 폴더 이름, 저장할 파일 이름
-                file.transferTo(saveFile); // 업로드 파일에 saveFile이라는 껍데기 입힘
-
-            }
-        }
-
-        Category findCategory = categoryRepository.findById(restaurantInfoDto.getCategoryId()).get();
-
-        findRestaurant.setAddress(restaurantInfoDto.getAddress());
-        findRestaurant.setName(restaurantInfoDto.getName());
-        findRestaurant.setOpenTime(restaurantInfoDto.getOpenTime());
-        findRestaurant.setCloseTime(restaurantInfoDto.getCloseTime());
-        findRestaurant.setCategory(findCategory);
-        findRestaurant.setFileName(saveName);
-//
-//        Restaurant restaurant = Restaurant.builder()
-//                .address(restaurantInfoDto.getAddress())
-//                .name(restaurantInfoDto.getName())
-//                .openTime(restaurantInfoDto.getOpenTime())
-//                .closeTime(restaurantInfoDto.getCloseTime())
-//                .category(findCategory)
-//                .fileName(saveName)
-//                .build();
-
-
-        restaurantRepository.save(findRestaurant);
+    public void editRestaurant(RestaurantInfoDto restaurantInfoDto) {
+        Category category = categoryService.getCategory(restaurantInfoDto.getCategoryId());
+        restaurantService.editRestaurant(restaurantInfoDto, category);
     }
 
     @DeleteMapping("/delete")
     public void deleteRestaurant(@RequestParam Long restaurantId) {
-        Restaurant findRestaurant = restaurantRepository.findById(restaurantId).get();
-        restaurantRepository.delete(findRestaurant);
+        restaurantService.deleteRestaurant(restaurantId);
     }
 }
