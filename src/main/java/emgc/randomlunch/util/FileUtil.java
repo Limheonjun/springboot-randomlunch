ea3f54159034bb9dae14660d44468960f69fbd79
limheonjun <jumong40@naver.com>
2021-03-13 11:30:34 +0900

    FileUtil 파일저장 로직 수정

--- src/main/java/emgc/randomlunch/util/FileUtil.java
+++ src/main/java/emgc/randomlunch/util/FileUtil.java
@@ -1,5 +1,6 @@
 package emgc.randomlunch.util;
 
+import emgc.randomlunch.dto.FileInfoDto;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -8,30 +9,36 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Component
 public class FileUtil {
 
-    public List<String> saveFile(MultipartFile files[], String path) throws IOException {
-        List<String> fileName = new ArrayList<>();
+    // 실제 파일 저장
+    public FileInfoDto saveFiles(MultipartFile file, String path) throws IOException {
+        FileInfoDto fileInfo = null;
 
-        for(MultipartFile file : files) {
-            BufferedImage image = ImageIO.read(file.getInputStream());
-            int width = image.getWidth();
-            int height = image.getHeight();
+        BufferedImage image = ImageIO.read(file.getInputStream());
+        int width = image.getWidth();
+        int height = image.getHeight();
+        String extension = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".") + 1);
 
-            // 파일 이름 변경
-            UUID uuid = UUID.randomUUID();
-            String saveName = uuid + "_" + LocalDate.now() + "." + file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".") + 1);
-            // 저장할 File 객체를 생성(껍데기 파일)
-            File saveFile = new File(path, saveName); // 저장할 폴더 이름, 저장할 파일 이름
-            file.transferTo(saveFile); // 업로드 파일에 saveFile이라는 껍데기 입힘
-            fileName.add(saveName);
-        }
 
-        return fileName;
+        // 파일 이름 변경
+        UUID uuid = UUID.randomUUID();
+        String saveName = uuid + "_" + LocalDate.now() + "." + extension;
+        // 저장할 File 객체를 생성(껍데기 파일)
+        File saveFile = new File(path, saveName); // 저장할 폴더 이름, 저장할 파일 이름
+        file.transferTo(saveFile); // 업로드 파일에 saveFile이라는 껍데기 입힘
+
+        fileInfo = FileInfoDto.builder()
+                            .height(height)
+                            .width(width)
+                            .name(saveName)
+                            .extension(extension)
+                            .build();
+
+
+        return fileInfo;
     }
 }
