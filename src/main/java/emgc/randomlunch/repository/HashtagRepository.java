5fe3d66fdf542c837018b7a53f85af944cb64223
limheonjun <jumong40@naver.com>
2021-03-13 12:27:02 +0900

    HashtagRepository 해시태그이름으로 찾기 추가

--- src/main/java/emgc/randomlunch/repository/HashtagRepository.java
+++ src/main/java/emgc/randomlunch/repository/HashtagRepository.java
@@ -3,5 +3,9 @@ package emgc.randomlunch.repository;
 import emgc.randomlunch.entity.Hashtag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
+
+    Optional<Hashtag> findByWord(String word);
 }
