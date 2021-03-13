ffa14e5b199e3a2030109c63deca9a448d1ab980
limheonjun <jumong40@naver.com>
2021-03-13 12:28:09 +0900

    ThumbnailApi 비즈니스 로직 분리

--- src/main/java/emgc/randomlunch/api/ThumbnailApi.java
+++ src/main/java/emgc/randomlunch/api/ThumbnailApi.java
@@ -1,5 +1,6 @@
 package emgc.randomlunch.api;
 
+import emgc.randomlunch.dto.FileInfoDto;
 import emgc.randomlunch.dto.ThumbnailInfoDto;
 import emgc.randomlunch.entity.Hashtag;
 import emgc.randomlunch.entity.Restaurant;
@@ -9,19 +10,15 @@ import emgc.randomlunch.repository.HashtagRepository;
 import emgc.randomlunch.repository.RestaurantRepository;
 import emgc.randomlunch.repository.ThumbnailHashtagRepository;
 import emgc.randomlunch.repository.ThumbnailRepository;
+import emgc.randomlunch.service.*;
+import emgc.randomlunch.entity.File;
+import emgc.randomlunch.util.FileUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -34,129 +31,66 @@ public class ThumbnailApi {
     private final RestaurantRepository restaurantRepository;
     private final HashtagRepository hashtagRepository;
     private final ThumbnailHashtagRepository thumbnailHashtagRepository;
+    private final ThumbnailService thumbnailService;
+    private final RestaurantService restaurantService;
+    private final HashtagService hashtagService;
+    private final FileService fileService;
+    private final ThumbnailHashtagService thumbnailHashtagService;
+    private final FileUtil fileUtil;
 
     @Value("${file.thumbnail.path}")
     private String path;
 
     @PostMapping("/upload")
-    public void uploadThumbnail(MultipartFile files[],
+    public void uploadThumbnailAndHashtag(MultipartFile files[],
                                 @RequestPart("restaurantId") Long restaurantId,
-                                @RequestPart("hashtags") String hashtags[],
-                                BindingResult bindingResult) throws IOException {
-        if(bindingResult.hasErrors()) {
-            bindingResult.getFieldErrors();
-            return;
-        }
+                                @RequestPart("hashtags") String hashtags[]
+                                ) throws IOException {
+
         List<Thumbnail> thumbnailList = new ArrayList<>();
-        List<Hashtag> hashtagList = new ArrayList<>();
+        List<File> fileList = new ArrayList<>();
+        Restaurant restaurant = restaurantService.getRestaurant(restaurantId);
 
-        // TODO : 음식점이 선택되지 않았을 경우 에러처리
-        Restaurant restaurant = restaurantRepository.findById(restaurantId).get();
+        // 해시태그 저장 및 Hashtag엔티티 생성
+        List<Hashtag> hashtagList = hashtagService.addHashtag(hashtags);
 
-        for (String hashtag : hashtags) {
-            // TODO : DB에 해시태그가 존재하는지 확인 후 있는 경우엔 DB에서 꺼내오기
-            Hashtag saveHashtag = hashtagRepository.save(Hashtag.builder().id(null).word(hashtag).build());
-            hashtagList.add(saveHashtag);
-        }
-
-        for (MultipartFile file : files) {
-
-            BufferedImage image = ImageIO.read(file.getInputStream());
-            int width = image.getWidth();
-            int height = image.getHeight();
-
-            // 파일 이름 변경
-            UUID uuid = UUID.randomUUID();
-            String saveName = uuid + "_" + LocalDate.now() + "." + file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".") + 1);
-            // 저장할 File 객체를 생성(껍데기 파일)
-            File saveFile = new File(path, saveName); // 저장할 폴더 이름, 저장할 파일 이름
-            file.transferTo(saveFile); // 업로드 파일에 saveFile이라는 껍데기 입힘
-
-            Thumbnail thumbnail = Thumbnail.builder()
-                    .id(null)
-                    .restaurant(restaurant)
-                    .size(saveFile.length())
-                    .thumbnailHeight(width)
-                    .thumbnailHeight(height)
-//                    .path(path+saveName)
-                    .fileName(saveName)
+        // 파일 저장 및 File엔티티 저장
+        for(MultipartFile file : files) {
+            FileInfoDto fileInfoDto = fileUtil.saveFile(file, path); // 실제 파일 저장
+            File addFile = File.builder()
+                    .name(fileInfoDto.getName())
+                    .height(fileInfoDto.getHeight())
+                    .width(fileInfoDto.getWidth())
+                    .extension(fileInfoDto.getExtension())
+                    .size(fileInfoDto.getSize())
                     .build();
-
-            Thumbnail saveThumbnail = thumbnailRepository.save(thumbnail);
-            thumbnailList.add(saveThumbnail);
+            File saveFile = fileService.addFile(addFile);
+            fileList.add(saveFile);
         }
 
+        // 썸네일 저장
+        for(File file : fileList) {
+            thumbnailList = thumbnailService.addThumbnail(file, restaurant);
+        }
+
+        // 썸네일 해시태그 연결
         for (Thumbnail thumbnail : thumbnailList) {
             for (Hashtag hashtag : hashtagList) {
-                ThumbnailHashtag thumbnailHashtag = ThumbnailHashtag.builder()
-                        .hashtag(hashtag)
-                        .thumbnail(thumbnail)
-                        .build();
-
-                thumbnailHashtagRepository.save(thumbnailHashtag);
+                thumbnailHashtagService.addThumnailHashtag(thumbnail, hashtag);
             }
         }
     }
 
     @GetMapping("/list")
     public List<ThumbnailInfoDto> getAllThumbnails(int pageNumber) {
-        System.out.println("pageNum : " + pageNumber);
-        Page<Thumbnail> all = thumbnailRepository.findAll(PageRequest.of(pageNumber, 25));
-//        all.stream().map(PostDTO::new).collect(Collectors.toList());
-//        List<Thumbnail> thumbnailList = (List<Thumbnail>) all;
-
-        List<ThumbnailInfoDto> thumbnailInfoDtoList = new ArrayList<>();
-
-        // TODO : thumbnail을 가져올때 hash를 1+n으로 가져오는 현상 eager처리로 변경 요망
-        for(Thumbnail thumbnail : all) {
-            ThumbnailInfoDto thumbnailInfoDto = ThumbnailInfoDto.builder()
-                    .thumbnailId(thumbnail.getId())
-                    .restaurantId(thumbnail.getRestaurant().getId())
-                    .restaurantName(thumbnail.getRestaurant().getName())
-                    .thumbnailHeight(thumbnail.getThumbnailHeight())
-//                    .path(thumbnail.getPath())
-                    .fileName(thumbnail.getFileName())
-                    .hashtags(thumbnail.getThumbnailHashtagList()
-                            .stream()
-                            .map(hashtag -> hashtag.getHashtag().getWord())
-                            .collect(Collectors.toList())
-                    )
-                    .build();
-
-            thumbnailInfoDtoList.add(thumbnailInfoDto);
-        }
-        Collections.shuffle(thumbnailInfoDtoList);
-
-        return thumbnailInfoDtoList;
+        return thumbnailService.getThumbnailList(pageNumber);
     }
 
     @GetMapping("/carousel")
-    public List<ThumbnailInfoDto> getRandomThumbnails(@RequestParam Long restaurantId) {
-        Restaurant findRestaurant = restaurantRepository.findById(restaurantId).get();
-        List<Thumbnail> thumbnailList = thumbnailRepository.findFirst8ByRestaurant(findRestaurant);
-        Collections.shuffle(thumbnailList);
-        List<ThumbnailInfoDto> thumbnailInfoDtoList = new ArrayList<>();
-
-        // TODO : thumbnail을 가져올때 hash를 1+n으로 가져오는 현상 eager처리로 변경 요망
-        for(Thumbnail thumbnail : thumbnailList) {
-            ThumbnailInfoDto thumbnailInfoDto = ThumbnailInfoDto.builder()
-                    .thumbnailId(thumbnail.getId())
-                    .restaurantId(thumbnail.getRestaurant().getId())
-                    .restaurantName(thumbnail.getRestaurant().getName())
-                    .thumbnailHeight(thumbnail.getThumbnailHeight())
-//                    .path(thumbnail.getPath())
-                    .fileName(thumbnail.getFileName())
-                    .hashtags(thumbnail.getThumbnailHashtagList()
-                            .stream()
-                            .map(hashtag -> hashtag.getHashtag().getWord())
-                            .collect(Collectors.toList())
-                    )
-                    .build();
-
-            thumbnailInfoDtoList.add(thumbnailInfoDto);
-        }
-
-        return thumbnailInfoDtoList;
+    public List<ThumbnailInfoDto> getCarousel(@RequestParam Long restaurantId) {
+        Restaurant findRestaurant = restaurantService.getRestaurant(restaurantId);
+        List<ThumbnailInfoDto> carousel = thumbnailService.getRandom8Thumbnail(findRestaurant);
+        return carousel;
     }
 
     // TODO : 썸네일 정보 조회 api 추가 요망
