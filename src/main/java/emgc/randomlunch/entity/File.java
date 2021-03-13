5ae494717bf709924dac3e9bbcedd8f50b69fb7b
limheonjun <jumong40@naver.com>
2021-03-13 10:57:22 +0900

    File엔티티 추가

---
+++ src/main/java/emgc/randomlunch/entity/File.java
@@ -0,0 +1,28 @@
+package emgc.randomlunch.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class File {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "file_id")
+    private Long id;
+
+    private Long size;
+    private int height;
+    private int width;
+    private int name;
+    private String extension;
+
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "file")
+    private Thumbnail thumbnail;
+}
