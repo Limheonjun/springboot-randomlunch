954411843731d533aea8d7bce6e33f2a17901582
limheonjun <jumong40@naver.com>
2021-05-08 17:06:20 +0900

    UPDATE : Question Entity Refactoring

--- src/main/java/emgc/randomlunch/entity/Question.java
+++ src/main/java/emgc/randomlunch/entity/Question.java
@@ -1,20 +1,26 @@
 package emgc.randomlunch.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Question {
 
     @Id
     @GeneratedValue
     @Column(name = "question_id")
     private Long id;
-
     private String email;
     private String content;
 }
