009037f1a5581359e523096e771b3b997b350154
limheonjun <jumong40@naver.com>
2021-05-08 17:09:17 +0900

    UPDATE : ThumbnailRepository Refactoring

--- src/main/java/emgc/randomlunch/repository/ThumbnailRepository.java
+++ src/main/java/emgc/randomlunch/repository/ThumbnailRepository.java
@@ -2,12 +2,8 @@ package emgc.randomlunch.repository;
 
 import emgc.randomlunch.entity.Restaurant;
 import emgc.randomlunch.entity.Thumbnail;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,6 +11,5 @@ public interface ThumbnailRepository extends JpaRepository<Thumbnail, Long> {
 
     List<Thumbnail> findFirst8ByRestaurant(Restaurant restaurant);
     List<Thumbnail> findByRestaurant(Restaurant restaurant, Pageable pageable);
-
 
 }
