2470b91dae44e72c6190f180abf0288841d080ed
limheonjun <jumong40@naver.com>
2021-05-08 17:11:25 +0900

    UPDATE : ThumbnailApi Refactoring

--- src/main/java/emgc/randomlunch/api/ThumbnailApi.java
+++ src/main/java/emgc/randomlunch/api/ThumbnailApi.java
@@ -1,101 +1,59 @@
 package emgc.randomlunch.api;
 
-import emgc.randomlunch.dto.FileInfoDto;
+import emgc.randomlunch.dto.RestaurantInfoDto;
 import emgc.randomlunch.dto.ThumbnailInfoDto;
+import emgc.randomlunch.entity.File;
 import emgc.randomlunch.entity.Hashtag;
 import emgc.randomlunch.entity.Restaurant;
 import emgc.randomlunch.entity.Thumbnail;
-import emgc.randomlunch.entity.ThumbnailHashtag;
-import emgc.randomlunch.repository.HashtagRepository;
-import emgc.randomlunch.repository.RestaurantRepository;
-import emgc.randomlunch.repository.ThumbnailHashtagRepository;
-import emgc.randomlunch.repository.ThumbnailRepository;
 import emgc.randomlunch.service.*;
-import emgc.randomlunch.entity.File;
-import emgc.randomlunch.util.FileUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/thumbnail")
 public class ThumbnailApi {
 
-    private final ThumbnailRepository thumbnailRepository;
-    private final RestaurantRepository restaurantRepository;
-    private final HashtagRepository hashtagRepository;
-    private final ThumbnailHashtagRepository thumbnailHashtagRepository;
     private final ThumbnailService thumbnailService;
     private final RestaurantService restaurantService;
     private final HashtagService hashtagService;
     private final FileService fileService;
     private final ThumbnailHashtagService thumbnailHashtagService;
-    private final FileUtil fileUtil;
 
     @Value("${file.thumbnail.path}")
     private String path;
 
     @PostMapping("/upload")
-    public void uploadThumbnailAndHashtag(MultipartFile files[],
-                                @RequestPart("restaurantId") Long restaurantId,
-                                @RequestPart("hashtags") String hashtags[]
-                                ) throws IOException {
-
-        List<Thumbnail> thumbnailList = new ArrayList<>();
-        List<File> fileList = new ArrayList<>();
-        Restaurant restaurant = restaurantService.getRestaurant(restaurantId);
-
-        // 해시태그 저장 및 Hashtag엔티티 생성
-        List<Hashtag> hashtagList = hashtagService.addHashtag(hashtags);
-
-        // 파일 저장 및 File엔티티 저장
-        for(MultipartFile file : files) {
-            FileInfoDto fileInfoDto = fileUtil.saveFile(file, path); // 실제 파일 저장
-            File addFile = File.builder()
-                    .name(fileInfoDto.getName())
-                    .height(fileInfoDto.getHeight())
-                    .width(fileInfoDto.getWidth())
-                    .extension(fileInfoDto.getExtension())
-                    .size(fileInfoDto.getSize())
-                    .build();
-            File saveFile = fileService.addFile(addFile);
-            fileList.add(saveFile);
-        }
-
-        // 썸네일 저장
-        for(File file : fileList) {
-            thumbnailList = thumbnailService.addThumbnail(file, restaurant);
-        }
-
-        // 썸네일 해시태그 연결
-        for (Thumbnail thumbnail : thumbnailList) {
-            for (Hashtag hashtag : hashtagList) {
-                thumbnailHashtagService.addThumnailHashtag(thumbnail, hashtag);
-            }
-        }
+    public void uploadThumbnailWithHashtag(@RequestPart("files")MultipartFile files[],
+                                           @RequestPart("restaurant")RestaurantInfoDto restaurantInfoDto) throws IOException {
+        Restaurant restaurant = restaurantService.getRestaurant(restaurantInfoDto);
+        List<Hashtag> hashtags = hashtagService.addHashtag(restaurantInfoDto.getHashtags().toArray(String[]::new));
+        List<File> fileList = fileService.addFileList(files, path);
+        List<Thumbnail> thumbnailList = thumbnailService.addThumbnailList(fileList, restaurant);
+        thumbnailHashtagService.addThumbnailHashtagList(thumbnailList, hashtags);
+    }
+    @GetMapping("/list")
+    public List<ThumbnailInfoDto> getThumbnailList(Pageable pageable) {
+        return thumbnailService.getThumbnailList(pageable);
     }
 
-    @GetMapping("/list")
-    public List<ThumbnailInfoDto> getAllThumbnails(int pageIndex, @RequestParam(required = false) Long restaurantId) {
-        Restaurant restaurant = null;
-        if(restaurantId != null) {
-            restaurant = restaurantService.getRestaurant(restaurantId);
-        }
-        return thumbnailService.getThumbnailList(pageIndex, restaurant);
+    @GetMapping("/restaurant")
+    public List<ThumbnailInfoDto> getThumbnailListByRestaurant(Pageable pageable, @RequestBody RestaurantInfoDto restaurantInfoDto) {
+        Restaurant restaurant = restaurantService.getRestaurant(restaurantInfoDto);
+        return thumbnailService.getThumbnailList(restaurant, pageable);
     }
 
     @GetMapping("/carousel")
-    public List<ThumbnailInfoDto> getCarousel(@RequestParam Long restaurantId) {
-        Restaurant findRestaurant = restaurantService.getRestaurant(restaurantId);
+    public List<ThumbnailInfoDto> getCarousel(@RequestBody RestaurantInfoDto restaurantInfoDto) {
+        Restaurant findRestaurant = restaurantService.getRestaurant(restaurantInfoDto);
         List<ThumbnailInfoDto> carousel = thumbnailService.getRandom8Thumbnail(findRestaurant);
         return carousel;
     }
-
-    // TODO : 썸네일 정보 조회 api 추가 요망
 }
