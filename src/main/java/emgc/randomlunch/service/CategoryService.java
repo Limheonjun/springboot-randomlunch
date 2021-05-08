5c10e4a7c2925470923fd70685c71911306caa7a
limheonjun <jumong40@naver.com>
2021-05-08 17:09:47 +0900

    UPDATE : CategoryService Refactoring

--- src/main/java/emgc/randomlunch/service/CategoryService.java
+++ src/main/java/emgc/randomlunch/service/CategoryService.java
@@ -6,9 +6,7 @@ import emgc.randomlunch.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -17,40 +15,34 @@ public class CategoryService {
 
     private final CategoryRepository repository;
 
-    //카테고리 목록 조회
     public List<CategoryInfoDto> getCategoryList() {
-        List<CategoryInfoDto> categoryList = new ArrayList<>();
         List<Category> findCategoryList = repository.findAll();
-        categoryList = findCategoryList.stream()
-                        .map(CategoryInfoDto::new)
-                        .collect(Collectors.toList());
-        return categoryList;
+        return findCategoryList.stream().map(CategoryInfoDto::new).collect(Collectors.toList());
     }
 
-    //카테고리 추가
-    public void addCategory(CategoryInfoDto categoryInfoDto) throws Exception{
-        //Optional<Category> optional = repository.findById(categoryInfoDto.getId());
-        //if(!optional.isEmpty()) throw new Exception();
+    public Category getCategory(CategoryInfoDto categoryInfoDto) {
+        return getCategory(categoryInfoDto.getId());
+    }
+
+    public Category getCategory(Long categoryId) {
+        return repository.findById(categoryId).orElseThrow();
+    }
+
+    public Category addCategory(CategoryInfoDto categoryInfoDto) {
         Category category = new Category(categoryInfoDto);
-        repository.save(category);
+        return repository.save(category);
     }
 
-    //카테고리 수정
-    public void editCategory(CategoryInfoDto categoryInfoDto) throws Exception {
+    public Category editCategory(CategoryInfoDto categoryInfoDto) {
         Category category = repository.findById(categoryInfoDto.getId()).orElseThrow();
         category.setName(categoryInfoDto.getName());
-        repository.save(category);
+        return category;
     }
 
-    //카테고리 삭제
-    public void deleteCategory(Long id) throws Exception {
-        Category category = repository.findById(id).orElseThrow();
+    public void deleteCategory(CategoryInfoDto categoryInfoDto) {
+        Category category = repository.findById(categoryInfoDto.getId()).orElseThrow();
         repository.delete(category);
     }
 
-    //카테고리 조회
-    public Category getCategory(Long id) {
-        Category category = repository.findById(id).orElseThrow();
-        return category;
-    }
+
 }
