9909938da0895c4da187b47f7f4186c3d2c3285e
limheonjun <jumong40@naver.com>
2021-05-08 17:10:09 +0900

    UPDATE : MenuService Refactoring

--- src/main/java/emgc/randomlunch/service/MenuService.java
+++ src/main/java/emgc/randomlunch/service/MenuService.java
@@ -1,13 +1,13 @@
 package emgc.randomlunch.service;
 
 import emgc.randomlunch.dto.MenuInfoDto;
+import emgc.randomlunch.dto.RestaurantInfoDto;
 import emgc.randomlunch.entity.Menu;
 import emgc.randomlunch.entity.Restaurant;
 import emgc.randomlunch.repository.MenuRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,40 +15,47 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class MenuService {
 
-    private final MenuRepository repository;
+    private final MenuRepository menuRepository;
+    private final RestaurantService restaurantService;
 
     //메뉴 목록 조회
     public List<MenuInfoDto> getMenuList(Restaurant restaurant) {
-        List<MenuInfoDto> menuList = new ArrayList<>();
-        List<Menu> findMenuList = repository.findByRestaurantOrderByPriceAsc(restaurant);
-        menuList = findMenuList.stream()
-                .map(MenuInfoDto::new)
-                .collect(Collectors.toList());
-        return menuList;
+        List<Menu> findMenuList = menuRepository.findByRestaurantOrderByPriceAsc(restaurant);
+        return findMenuList.stream().map(MenuInfoDto::new).collect(Collectors.toList());
+    }
+
+    public List<MenuInfoDto> getMenuList(RestaurantInfoDto restaurantInfoDto) {
+        Restaurant restaurant = restaurantService.getRestaurant(restaurantInfoDto.getId());
+        return getMenuList(restaurant);
     }
 
     //메뉴 추가
-    public void addMenu(Menu menu) {
-        repository.save(menu);
+    public Menu addMenu(MenuInfoDto menuInfoDto) {
+        Restaurant restaurant = restaurantService.getRestaurant(menuInfoDto.getRestaurantId());
+        Menu menu = Menu.builder()
+                .restaurant(restaurant)
+                .price(menuInfoDto.getPrice())
+                .name(menuInfoDto.getName())
+                .build();
+        return menuRepository.save(menu);
     }
 
     //메뉴 수정
-    public void editMenu(MenuInfoDto menuInfoDto) {
-        Menu menu = getMenu(menuInfoDto.getId());
+    public Menu editMenu(MenuInfoDto menuInfoDto) {
+        Menu menu = getMenu(menuInfoDto);
         menu.setName(menuInfoDto.getName());
         menu.setPrice(menuInfoDto.getPrice());
-        repository.save(menu);
+        return menu;
     }
 
     //메뉴 삭제
-    public void deleteMenu(Long id) {
-        Menu menu = repository.findById(id).orElseThrow();
-        repository.delete(menu);
+    public void deleteMenu(MenuInfoDto menuInfoDto) {
+        Menu menu = menuRepository.findById(menuInfoDto.getId()).orElseThrow();
+        menuRepository.delete(menu);
     }
 
     //메뉴 찾기
-    public Menu getMenu(Long id) {
-        Menu menu = repository.findById(id).orElseThrow();
-        return menu;
+    public Menu getMenu(MenuInfoDto menuInfoDto) {
+        return menuRepository.findById(menuInfoDto.getId()).orElseThrow();
     }
 }
