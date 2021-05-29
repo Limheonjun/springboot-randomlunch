db23cf77a6bb3def99debe5ffe0f5c215bd6cb4a
o,hepmkim <jumong40@naver.com>
2021-05-29 21:44:32 +0900

    UPDATE : add spring security default config

--- src/main/java/emgc/randomlunch/security/config/SecurityConfig.java
+++ src/main/java/emgc/randomlunch/security/config/SecurityConfig.java
@@ -2,12 +2,16 @@ package emgc.randomlunch.security.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .
+                .cors().disable()
+                .csrf().disable()
+                .formLogin().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
