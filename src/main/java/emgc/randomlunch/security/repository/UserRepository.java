5ab9740a93bc6c9520ce820ab0b6fe8a8a9605ad
o,hepmkim <jumong40@naver.com>
2021-06-04 21:31:43 +0900

    ADD : UserRepository

---
+++ src/main/java/emgc/randomlunch/security/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package emgc.randomlunch.security.repository;
+
+import emgc.randomlunch.security.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
