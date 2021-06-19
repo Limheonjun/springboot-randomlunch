c585f06c7142854d7be7972ca6acc93fd10cf898
Limheonjun <jumong40@naver.com>
2021-06-19 18:35:51 +0900

    U : add a method that returns a user

--- src/main/java/emgc/randomlunch/security/domain/User.java
+++ src/main/java/emgc/randomlunch/security/domain/User.java
@@ -1,5 +1,6 @@
 package emgc.randomlunch.security.domain;
 
+import emgc.randomlunch.dto.UserDto;
 import emgc.randomlunch.enums.Gender;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -72,5 +73,14 @@ public class User implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    public User updateInfo(UserDto userDto){
+        this.email = userDto.getEmail();
+        this.password = userDto.getPassword();
+        this.phoneNumber = userDto.getPhoneNumber();
+        this.name = userDto.getName();
+        this.gender = userDto.getGender();
+        return this;
     }
 }
