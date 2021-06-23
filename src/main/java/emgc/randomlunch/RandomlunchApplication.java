7757ae998186287b63b4628ccbd6bd1ef24cd47d
Limheonjun <jumong40@naver.com>
2021-06-23 12:05:12 +0900

    U : enable jpa auditing

--- src/main/java/emgc/randomlunch/RandomlunchApplication.java
+++ src/main/java/emgc/randomlunch/RandomlunchApplication.java
@@ -2,9 +2,11 @@ package emgc.randomlunch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RandomlunchApplication {
 
     public static void main(String[] args) {
