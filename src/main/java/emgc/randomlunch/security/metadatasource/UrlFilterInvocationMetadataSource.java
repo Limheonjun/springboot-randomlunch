d332dca15c10a516427a546675fd7c66fa5cec5d
o,hepmkim <jumong40@naver.com>
2021-06-26 20:52:06 +0900

    C : add UrlFilterInvocationMetadataSource

---
+++ src/main/java/emgc/randomlunch/security/metadatasource/UrlFilterInvocationMetadataSource.java
@@ -0,0 +1,52 @@
+package emgc.randomlunch.security.metadatasource;
+
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+
+/**
+ * 권한정보 추출하는 역할
+ */
+@Component
+public class UrlFilterInvocationMetadataSource implements FilterInvocationSecurityMetadataSource {
+
+    private LinkedHashMap<RequestMatcher, List<ConfigAttribute>> requestMap = new LinkedHashMap<>();
+
+    public UrlFilterInvocationMetadataSource(LinkedHashMap<RequestMatcher, List<ConfigAttribute>> requestMap) {
+        this.requestMap = requestMap;
+    }
+
+    @Override
+    public Collection<ConfigAttribute> getAttributes(Object object) throws IllegalArgumentException {
+
+        HttpServletRequest request = ((FilterInvocation) object).getRequest();
+
+        if(requestMap != null) {
+            for(Map.Entry<RequestMatcher, List<ConfigAttribute>> entry : requestMap.entrySet()){
+                RequestMatcher matcher = entry.getKey();
+                if(matcher.matches(request)){
+                    return entry.getValue();
+                }
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public Collection<ConfigAttribute> getAllConfigAttributes() {
+        Set<ConfigAttribute> allAttributes = new HashSet<>();
+        this.requestMap.values().forEach(allAttributes::addAll);
+        return allAttributes;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return FilterInvocation.class.isAssignableFrom(clazz);
+    }
+}
