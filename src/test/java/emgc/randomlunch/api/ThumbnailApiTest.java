fa93d2118c31a90ee41b012673dc445e1f5ce66a
o,hepmkim <jumong40@naver.com>
2021-07-16 17:38:36 +0900

    C : add UserApiTest

---
+++ src/test/java/emgc/randomlunch/api/ThumbnailApiTest.java
@@ -0,0 +1,28 @@
+package emgc.randomlunch.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ThumbnailApiTest {
+
+    @Test
+    void uploadThumbnailWithHashtag() {
+    }
+
+    @Test
+    void getThumbnailList() {
+    }
+
+    @Test
+    void getThumbnailListByUser() {
+    }
+
+    @Test
+    void getThumbnailListByRestaurant() {
+    }
+
+    @Test
+    void getCarousel() {
+    }
+}
