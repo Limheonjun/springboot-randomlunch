001e726306d31d1c7bfa43e0ced287a467c7efa9
o,hepmkim <jumong40@naver.com>
2021-07-16 17:32:55 +0900

    U : edit QuestionApi parameter

--- src/main/java/emgc/randomlunch/api/QuestionApi.java
+++ src/main/java/emgc/randomlunch/api/QuestionApi.java
@@ -1,19 +1,22 @@
 package emgc.randomlunch.api;
 
-import emgc.randomlunch.entity.Question;
-import emgc.randomlunch.repository.QuestionRepository;
+import emgc.randomlunch.dto.QuestionInfoDto;
+import emgc.randomlunch.service.QuestionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/question")
 @RequiredArgsConstructor
 public class QuestionApi {
 
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
     @PostMapping("/upload")
-    public void uploadQuestion(@RequestBody  Question question) {
-        questionRepository.save(question);
+    public void uploadQuestion(@RequestBody QuestionInfoDto questionInfoDto) {
+        questionService.uploadQuestion(questionInfoDto);
     }
 }
