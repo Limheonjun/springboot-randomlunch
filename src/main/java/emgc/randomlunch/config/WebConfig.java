7523cb335bdcd9d539ac47e14066542136f11116
o,hepmkim <jumong40@naver.com>
2021-07-20 10:33:45 +0900

    U : add allow methods in cors setting

--- src/main/java/emgc/randomlunch/config/WebConfig.java
+++ src/main/java/emgc/randomlunch/config/WebConfig.java
@@ -3,6 +3,7 @@ package emgc.randomlunch.config;
 import emgc.randomlunch.interceptor.UrlPathInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -23,6 +24,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .exposedHeaders("X-AUTH-TOKEN")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.DELETE.name()
+                )
                 .allowCredentials(true)
                 .allowedOrigins("http://localhost:3000", "https://react-randomlunch.vercel.app");
     }
