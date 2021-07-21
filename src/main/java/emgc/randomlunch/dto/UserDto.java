3939698cf68f36eb810b917ec552262168d255c0
o,hepmkim <jumong40@naver.com>
2021-07-21 16:30:50 +0900

    U : edit userDto

--- src/main/java/emgc/randomlunch/dto/UserDto.java
+++ src/main/java/emgc/randomlunch/dto/UserDto.java
@@ -22,7 +22,7 @@ public class UserDto {
 
     private String password;
 
-    @NotBlank(message = "이름은 필수값 입니다.")
+//    @NotBlank(message = "이름은 필수값 입니다.")
     private String name;
 
     private String phoneNumber;
