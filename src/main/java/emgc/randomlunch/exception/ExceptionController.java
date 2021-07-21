e469d973d815e3c2e26efcbaa110c54c5ce963e4
o,hepmkim <jumong40@naver.com>
2021-07-21 16:32:24 +0900

    U : add ExceptionController

---
+++ src/main/java/emgc/randomlunch/exception/ExceptionController.java
@@ -0,0 +1,29 @@
+package emgc.randomlunch.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+
+@RestControllerAdvice
+@Slf4j
+public class ExceptionController {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity methodValidationException(
+            MethodArgumentNotValidException e,
+            HttpServletRequest request,
+            Principal principal
+    ){
+        String user = principal == null ? "anonymous" : principal.getName();
+        String message = e.getAllErrors().get(0).getDefaultMessage();
+        log.info("MethodArgumentNotValidException occured | user : {} , url : {} , trace : {}", user, request.getRequestURI(), e.getStackTrace());
+        log.info("Error Message : {}", message);
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
+}
