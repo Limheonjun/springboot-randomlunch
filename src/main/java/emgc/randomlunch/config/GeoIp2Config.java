d068932d84c9ce3f017b39ac6f5eed7a2e2a70ab
o,hepmkim <jumong40@naver.com>
2021-07-23 17:49:39 +0900

    U : edit geoip2 db location

--- src/main/java/emgc/randomlunch/config/GeoIp2Config.java
+++ src/main/java/emgc/randomlunch/config/GeoIp2Config.java
@@ -2,6 +2,7 @@ package emgc.randomlunch.config;
 
 import com.maxmind.geoip2.DatabaseReader;
 import com.maxmind.geoip2.exception.GeoIp2Exception;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,9 +12,12 @@ import java.io.IOException;
 @Configuration
 public class GeoIp2Config {
 
+    @Value("${geoip2.database.path}")
+    private String path;
+
     @Bean
     public DatabaseReader databaseReader() throws IOException, GeoIp2Exception{
-        File resource = new File("src/main/resources/GeoLite2-Country.mmdb");
+        File resource = new File("file:///" + path);
         return new DatabaseReader.Builder(resource).build();
     }
 }
