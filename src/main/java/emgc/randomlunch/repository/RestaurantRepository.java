ebb68fda3eb15e5b39f0caaa9f6a58574a82b7fc
Limheonjun <jumong40@naver.com>
2021-12-04 15:13:32 +0900

    feat: findAllByNameLike메소드 추가

--- src/main/java/emgc/randomlunch/repository/RestaurantRepository.java
+++ src/main/java/emgc/randomlunch/repository/RestaurantRepository.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import emgc.randomlunch.entity.Category;
 import emgc.randomlunch.entity.Restaurant;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
-	List<Restaurant> findByCategory(Category category);
+	List<Restaurant> findAllByNameLike(String name);
+
 }
