bf07df50958a8dc8c7ce35b64e125df455e1bc1e
Limheonjun <jumong40@naver.com>
2021-12-04 15:18:22 +0900

    rename: UserRepository경로 변경

---
+++ src/main/java/emgc/randomlunch/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package emgc.randomlunch.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import emgc.randomlunch.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByEmail(String email);
+
+}
