30319be236ace8a7b0eb246a48011417f46d0623
Limheonjun <jumong40@naver.com>
2021-12-04 15:10:59 +0900

    refactor: ThumbnailRepository 조건에 따른 썸네일 검색 쿼리 메소드 추가

--- src/main/java/emgc/randomlunch/repository/ThumbnailRepository.java
+++ src/main/java/emgc/randomlunch/repository/ThumbnailRepository.java
@@ -5,16 +5,56 @@ import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import emgc.randomlunch.entity.Category;
 import emgc.randomlunch.entity.Restaurant;
 import emgc.randomlunch.entity.Thumbnail;
-import emgc.randomlunch.security.domain.User;
+import emgc.randomlunch.entity.ThumbnailHashtag;
+import emgc.randomlunch.entity.User;
 
 public interface ThumbnailRepository extends JpaRepository<Thumbnail, Long> {
-
-	List<Thumbnail> findFirst8ByRestaurant(Restaurant restaurant);
 
 	List<Thumbnail> findByRestaurant(Restaurant restaurant, Pageable pageable);
 
 	List<Thumbnail> findByUser(User user, Pageable pageable);
+
+	/**
+	 * 전체 카테고리에서 음식점 명이 포함된 것을 기준으로 검색 최신순(기본옵션)
+	 * RestaurantRepository.findAllByNameLike를 통해 음식점 먼저 검색
+	 */
+	List<Thumbnail> findAllByRestaurantInOrderByCreateDateDesc(
+		List<Restaurant> restaurantList,
+		Pageable pageable
+	);
+
+	/**
+	 * 전체 카테고리에서 해시태그 명이 포함된 것을 기준으로 검색 최신순(기본옵션)
+	 * HashtagRepository.findAllByWordLike를 통해 해시태그 검색
+	 * ThumbnailHashtagRepository.findAllByHashtagIn을 통해 해시태그가 포함된 튜플 선택
+	 */
+	List<Thumbnail> findAllByThumbnailHashtagInOrderByCreateDateDesc(
+		List<ThumbnailHashtag> thumbnailHashtagList,
+		Pageable pageable
+	);
+
+	/**
+	 * 특정 카테고리에서 음식점 명이 포함된 것을 기준으로 검색 최신순(기본옵션)
+	 * RestaurantRepository.findAllByNameLike를 통해 음식점 먼저 검색
+	 */
+	List<Thumbnail> findAllByCategoryAndRestaurantInOrderByCreateDateDesc(
+		Category category,
+		List<Restaurant> restaurantList,
+		Pageable pageable
+	);
+
+	/**
+	 * 전체 카테고리에서 해시태그 명이 포함된 것을 기준으로 검색 최신순(기본옵션)
+	 * HashtagRepository.findAllByWordLike를 통해 해시태그 검색
+	 * ThumbnailHashtagRepository.findAllByHashtagIn을 통해 해시태그가 포함된 튜플 선택
+	 */
+	List<Thumbnail> findAllByCategoryAndThumbnailHashtagInOrderByCreateDateDesc(
+		Category category,
+		List<ThumbnailHashtag> thumbnailHashtagList,
+		Pageable pageable
+	);
 
 }
