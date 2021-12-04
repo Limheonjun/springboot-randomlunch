4b02058e885ea02c6ec8d08793d4612d59d28114
Limheonjun <jumong40@naver.com>
2021-12-04 15:11:31 +0900

    remove: FileRepository, MenuRepository 제거

--- src/main/java/emgc/randomlunch/repository/FileRepository.java
+++
@@ -1,8 +0,0 @@
-package emgc.randomlunch.repository;
-
-import emgc.randomlunch.entity.File;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<File, Long> {
-}
