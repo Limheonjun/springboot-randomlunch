df1570190d0b97cf1a3422b02e60309594c45e50
Limheonjun <jumong40@naver.com>
2021-12-08 00:25:19 +0900

    feat: User타입을 인자로 받는 정적 팩토리 메소드 from 추가

--- src/main/java/emgc/randomlunch/dto/LoginResponse.java
+++ src/main/java/emgc/randomlunch/dto/LoginResponse.java
@@ -1,5 +1,6 @@
 package emgc.randomlunch.dto;
 
+import emgc.randomlunch.entity.User;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,5 +9,10 @@ import lombok.Getter;
 public class LoginResponse {
 
 	private String email;
+
+	public static LoginResponse from(User user) {
+		String email = user.getEmail();
+		return LoginResponse.builder().email(email).build();
+	}
 
 }
