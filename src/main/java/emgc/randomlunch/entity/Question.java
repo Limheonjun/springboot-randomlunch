11f7223bd734f8f037e9f5d46a1e7e7d20797f34
Limheonjun <jumong40@naver.com>
2021-12-08 00:20:21 +0900

    feat: title속성 및 User엔티티와 연관관계 추가

--- src/main/java/emgc/randomlunch/entity/Question.java
+++ src/main/java/emgc/randomlunch/entity/Question.java
@@ -1,11 +1,19 @@
 package emgc.randomlunch.entity;
 
-import lombok.*;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import emgc.randomlunch.dto.QuestionRequest;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -19,6 +27,20 @@ public class Question extends BaseEntity {
 	@GeneratedValue
 	@Column(name = "question_id")
 	private Long id;
+
+	private String title;
+
 	private String content;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	public static Question of(QuestionRequest request, User user) {
+		String title = request.getTitle();
+		String content = request.getContent();
+
+		return Question.builder().title(title).content(content).user(user).build();
+	}
 
 }
