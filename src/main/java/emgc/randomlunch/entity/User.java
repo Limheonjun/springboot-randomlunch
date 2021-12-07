1df1b875ee19620e0aae6a19ecf3eeca228f88d4
Limheonjun <jumong40@naver.com>
2021-12-08 00:32:47 +0900

    refactor: UserDetails구현 제거 및 코드 포맷팅

--- src/main/java/emgc/randomlunch/entity/User.java
+++ src/main/java/emgc/randomlunch/entity/User.java
@@ -1,8 +1,6 @@
 package emgc.randomlunch.entity;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -14,11 +12,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import emgc.randomlunch.dto.UserDto;
 import emgc.randomlunch.enums.Role;
 import emgc.randomlunch.enums.ServiceProvider;
 import lombok.AllArgsConstructor;
@@ -31,67 +24,29 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-public class User extends BaseTimeEntity implements UserDetails{
+public class User extends BaseTimeEntity {
 
-    @Id
-    @GeneratedValue
-    @Column(name = "user_id")
-    private Long id;
+	@Id
+	@GeneratedValue
+	@Column(name = "user_id")
+	private Long id;
 
-    @Enumerated(EnumType.STRING)
-    private ServiceProvider serviceProvider;
+	@Enumerated(EnumType.STRING)
+	private ServiceProvider serviceProvider;
 
-    @Enumerated(EnumType.STRING)
-    private Role role;
+	@Enumerated(EnumType.STRING)
+	private Role role;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Thumbnail> thumbnailList = new ArrayList<>();
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Thumbnail> thumbnailList = new ArrayList<>();
 
-    @OneToMany(mappedBy = "user")
-    private List<Question> questionList = new ArrayList<>();
+	@OneToMany(mappedBy = "user")
+	private List<Question> questionList = new ArrayList<>();
 
-    private String email;
-    private String password;
-    private String nickname;
+	private String email;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Arrays.asList(new SimpleGrantedAuthority(role.name()));
-    }
+	private String password;
 
-    @Override
-    public String getPassword() {
-        return password;
-    }
+	private String nickname;
 
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-
-    public User updateInfo(UserDto userDto){
-        this.email = userDto.getEmail();
-        this.password = userDto.getPassword();
-        return this;
-    }
 }
