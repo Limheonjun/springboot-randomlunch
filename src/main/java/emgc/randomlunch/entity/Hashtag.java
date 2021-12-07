e7b14d2366f4ccf5edee4c8bc4745b81ebb98fe6
Limheonjun <jumong40@naver.com>
2021-12-08 00:22:43 +0900

    refactor: 커스텀 생성자 제거

--- src/main/java/emgc/randomlunch/entity/Hashtag.java
+++ src/main/java/emgc/randomlunch/entity/Hashtag.java
@@ -1,33 +1,35 @@
 package emgc.randomlunch.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Hashtag extends BaseEntity{
+public class Hashtag extends BaseEntity {
 
-    @Id
-    @GeneratedValue
-    @Column(name = "hashtag_id")
-    private Long id;
+	@Id
+	@GeneratedValue
+	@Column(name = "hashtag_id")
+	private Long id;
 
-    private String word;
+	private String word;
 
-    @OneToMany(mappedBy = "hashtag", cascade = CascadeType.ALL)
-    private List<ThumbnailHashtag> thumbnailHashtagList = new ArrayList<>();
-
-    public Hashtag(String word){
-        this.word = word;
-    }
+	@OneToMany(mappedBy = "hashtag", cascade = CascadeType.ALL)
+	private List<ThumbnailHashtag> thumbnailHashtagList = new ArrayList<>();
 
 }
