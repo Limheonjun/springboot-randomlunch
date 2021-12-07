fa204b8e345f0feea08ac67fb492fc9fdead67d8
Limheonjun <jumong40@naver.com>
2021-12-08 00:13:18 +0900

    refacotr: QuestionRequest 불필요한 메소드 및 속성 제거
    - id, email속성 제거
    - title속성 추가
    - 커스텀 생성자 제거

---
+++ src/main/java/emgc/randomlunch/dto/QuestionRequest.java
@@ -0,0 +1,25 @@
+package emgc.randomlunch.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuestionRequest {
+
+	private String title;
+
+	@NotBlank(message = "내용은 필수값 입니다.")
+	@Size(min = 1, max = 500, message = "내용의 길이는 1~500자 이어야 합니다.")
+	private String content;
+
+}
