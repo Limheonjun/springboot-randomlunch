f4fd3aeefd6aff9134e9ccad6dce0397f70183c3
Limheonjun <jumong40@naver.com>
2021-12-08 00:20:03 +0900

    feat: User엔티티 Question엔티티와 연관관계 추가

--- src/main/java/emgc/randomlunch/entity/User.java
+++ src/main/java/emgc/randomlunch/entity/User.java
@@ -1,21 +1,30 @@
 package emgc.randomlunch.entity;
 
-import emgc.randomlunch.dto.UserDto;
-import emgc.randomlunch.enums.Gender;
-import emgc.randomlunch.enums.Role;
-import emgc.randomlunch.enums.ServiceProvider;
-import emgc.randomlunch.security.domain.UserRole;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.*;
-import java.util.*;
-import java.util.stream.Collectors;
+import emgc.randomlunch.dto.UserDto;
+import emgc.randomlunch.enums.Role;
+import emgc.randomlunch.enums.ServiceProvider;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Builder
@@ -37,6 +46,9 @@ public class User extends BaseTimeEntity implements UserDetails{
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Thumbnail> thumbnailList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<Question> questionList = new ArrayList<>();
 
     private String email;
     private String password;
