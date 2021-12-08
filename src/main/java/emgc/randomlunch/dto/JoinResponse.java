4fde742e7dcba7c773ad30fd7832b462e09846cd
Limheonjun <jumong40@naver.com>
2021-12-09 00:38:09 +0900

    feat: JoinResponse 추가

---
+++ src/main/java/emgc/randomlunch/dto/JoinResponse.java
@@ -0,0 +1,22 @@
+package emgc.randomlunch.dto;
+
+import emgc.randomlunch.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class JoinResponse {
+
+	private String email;
+
+	private String role;
+
+	public static JoinResponse from(User user) {
+		String email = user.getEmail();
+		String role = user.getRole().name();
+
+		return JoinResponse.builder().email(email).role(role).build();
+	}
+
+}
