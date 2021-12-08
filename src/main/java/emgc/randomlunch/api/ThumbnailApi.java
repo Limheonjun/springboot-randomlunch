f98b54addd2931e6748894eb8f8a758c30f9e64b
Limheonjun <jumong40@naver.com>
2021-12-09 00:17:39 +0900

    feat: ThumbnailApi 기능 추가
    - 음식점 이름으로 썸네일 조회
    - 해시태그로 썸네일 조회
    - 카테고리 및 음식점 이름으로 썸네일 조회
    - 카테고리 및 해시태그로 썸네일 조회

--- src/main/java/emgc/randomlunch/api/ThumbnailApi.java
+++ src/main/java/emgc/randomlunch/api/ThumbnailApi.java
@@ -1,8 +1,14 @@
 package emgc.randomlunch.api;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import emgc.randomlunch.dto.ThumbnailResponse;
 import emgc.randomlunch.service.function.ThumbnailService;
 import lombok.RequiredArgsConstructor;
 
@@ -12,5 +18,40 @@ import lombok.RequiredArgsConstructor;
 public class ThumbnailApi {
 
 	private ThumbnailService thumbnailService;
+
+	//정렬이나 필터링의 경우에는 QueryString 사용
+	@GetMapping(value = "", params = {"restaurantName", "page", "size"})
+	public List<ThumbnailResponse> getThumbnailListByRestaurantName(
+		@RequestParam String restaurantName,
+		@RequestParam Pageable pageable
+	) {
+		return thumbnailService.getThumbnailListByRestaurantName(restaurantName, pageable);
+	}
+
+	@GetMapping(value = "", params = {"keyword", "page", "size"})
+	public List<ThumbnailResponse> getThumbnailListByHashtag(
+		@RequestParam String keyword,
+		@RequestParam Pageable pageable
+	) {
+		return thumbnailService.getThumbnailListByHashtag(keyword, pageable);
+	}
+
+	@GetMapping(value = "", params = {"categoryId", "restaurantName", "page", "size"})
+	public List<ThumbnailResponse> getThumbnailListByCategoryAndRestaurantName(
+		@RequestParam Long categoryId,
+		@RequestParam String restaurantName,
+		@RequestParam Pageable pageable
+	) {
+		return thumbnailService.getThumbnailListByCategoryAndRestaurantName(categoryId, restaurantName, pageable);
+	}
+
+	@GetMapping(value = "", params = {"categoryId", "keyword", "page", "size"})
+	public List<ThumbnailResponse> getThumbnailListByCategoryAndHashtag(
+		@RequestParam Long categoryId,
+		@RequestParam String keyword,
+		@RequestParam Pageable pageable
+	) {
+		return thumbnailService.getThumbnailListByCategoryAndHashtag(categoryId, keyword, pageable);
+	}
 
 }
