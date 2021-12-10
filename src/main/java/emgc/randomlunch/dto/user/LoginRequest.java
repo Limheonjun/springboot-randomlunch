ff4a5b6e5c7058b0faa054717bf6d0793e0233bc
Limheonjun <jumong40@naver.com>
2021-12-10 18:49:54 +0900

    refactor: 제약조건 추가

--- src/main/java/emgc/randomlunch/dto/user/LoginRequest.java
+++ src/main/java/emgc/randomlunch/dto/user/LoginRequest.java
@@ -1,5 +1,9 @@
 package emgc.randomlunch.dto.user;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,8 +11,13 @@ import lombok.Getter;
 @Builder
 public class LoginRequest {
 
+	@NotBlank(message = "이메일은 필수 입력 값입니다.")
+	@Email(message = "이메일 형식에 맞지 않습니다.")
+	@Size(max = 255, message = "이메일은 최대 255자까지 입력할 수 있습니다.")
 	private String email;
 
+	@NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+	@Size(min = 8, max = 20, message = "비밀번호는 8~20자 사이여야 합니다.")
 	private String password;
 
 }
