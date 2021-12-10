8f3f0993fb4645b211b16167fdad80ef737c7410
Limheonjun <jumong40@naver.com>
2021-12-10 19:22:50 +0900

    refactor: title 유효성 검사 조건 추가

--- src/main/java/emgc/randomlunch/dto/question/QuestionRequest.java
+++ src/main/java/emgc/randomlunch/dto/question/QuestionRequest.java
@@ -16,6 +16,8 @@ import lombok.Setter;
 @AllArgsConstructor
 public class QuestionRequest {
 
+	@NotBlank(message = "제목은 필수값 입니다.")
+	@Size(min = 1, max = 30, message = "제목의 길이는 1~30자 사이여야 합니다.")
 	private String title;
 
 	@NotBlank(message = "내용은 필수값 입니다.")
