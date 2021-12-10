a74e47c3070a7d3a243cb0cc32ee5a99e14e4f77
Limheonjun <jumong40@naver.com>
2021-12-10 18:49:27 +0900

    refactor: QuestionRequest 최대 글자 수 수정

--- src/main/java/emgc/randomlunch/dto/question/QuestionRequest.java
+++ src/main/java/emgc/randomlunch/dto/question/QuestionRequest.java
@@ -19,7 +19,7 @@ public class QuestionRequest {
 	private String title;
 
 	@NotBlank(message = "내용은 필수값 입니다.")
-	@Size(min = 1, max = 500, message = "내용의 길이는 1~500자 이어야 합니다.")
+	@Size(min = 1, max = 255, message = "내용의 길이는 1~255자 사이여야 합니다.")
 	private String content;
 
 }
