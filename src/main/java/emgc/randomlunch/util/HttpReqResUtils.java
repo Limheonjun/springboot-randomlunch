48a3dc03ec67f442fd55c45aaa33dccbc1d5cac5
Limheonjun <jumong40@naver.com>
2021-12-10 19:23:16 +0900

    style: 코드 스타일 정리

--- src/main/java/emgc/randomlunch/util/HttpReqResUtils.java
+++ src/main/java/emgc/randomlunch/util/HttpReqResUtils.java
@@ -1,41 +1,41 @@
 package emgc.randomlunch.util;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
-
 public class HttpReqResUtils {
 
-    private static final String[] IP_HEADER_CANDIDATES = {
-            "X-Forwarded-For",
-            "Proxy-Client-IP",
-            "WL-Proxy-Client-IP",
-            "HTTP_X_FORWARDED_FOR",
-            "HTTP_X_FORWARDED",
-            "HTTP_X_CLUSTER_CLIENT_IP",
-            "HTTP_CLIENT_IP",
-            "HTTP_FORWARDED_FOR",
-            "HTTP_FORWARDED",
-            "HTTP_VIA",
-            "REMOTE_ADDR"
-    };
+	private static final String[] IP_HEADER_CANDIDATES = {
+		"X-Forwarded-For",
+		"Proxy-Client-IP",
+		"WL-Proxy-Client-IP",
+		"HTTP_X_FORWARDED_FOR",
+		"HTTP_X_FORWARDED",
+		"HTTP_X_CLUSTER_CLIENT_IP",
+		"HTTP_CLIENT_IP",
+		"HTTP_FORWARDED_FOR",
+		"HTTP_FORWARDED",
+		"HTTP_VIA",
+		"REMOTE_ADDR"
+	};
 
-    public static String getClientIpAddressIfServletRequestExist() {
+	public static String getClientIpAddressIfServletRequestExist() {
 
-        if (RequestContextHolder.getRequestAttributes() == null) {
-            return "0.0.0.0";
-        }
+		if (RequestContextHolder.getRequestAttributes() == null) {
+			return "0.0.0.0";
+		}
 
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        for (String header: IP_HEADER_CANDIDATES) {
-            String ipList = request.getHeader(header);
-            if (ipList != null && ipList.length() != 0 && !"unknown".equalsIgnoreCase(ipList)) {
-                String ip = ipList.split(",")[0];
-                return ip;
-            }
-        }
+		HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+		for (String header : IP_HEADER_CANDIDATES) {
+			String ipList = request.getHeader(header);
+			if (ipList != null && ipList.length() != 0 && !"unknown".equalsIgnoreCase(ipList)) {
+				String ip = ipList.split(",")[0];
+				return ip;
+			}
+		}
 
-        return request.getRemoteAddr();
-    }
+		return request.getRemoteAddr();
+	}
 }
