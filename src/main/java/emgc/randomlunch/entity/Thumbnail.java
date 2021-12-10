40ee3684dead923552ef2a1406fbbb981908564a
Limheonjun <jumong40@naver.com>
2021-12-10 21:11:34 +0900

    feat: Thumbnail 정적 팩토리 메소드 of 추가

--- src/main/java/emgc/randomlunch/entity/Thumbnail.java
+++ src/main/java/emgc/randomlunch/entity/Thumbnail.java
@@ -1,5 +1,7 @@
 package emgc.randomlunch.entity;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +15,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import emgc.randomlunch.util.FileUtil;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -42,7 +47,7 @@ public class Thumbnail extends BaseEntity {
 	@JoinColumn(name = "id")
 	private User user;
 
-	@OneToMany(mappedBy = "thumbnail", cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "thumbnail", cascade = CascadeType.PERSIST)
 	private List<ThumbnailHashtag> thumbnailHashtagList = new ArrayList<>();
 
 	private Long size;
@@ -51,14 +56,43 @@ public class Thumbnail extends BaseEntity {
 
 	private Integer width;
 
-	private Long likes;
-
-	private Long views;
-
 	private String originalFileName;
 
 	private String fileName;
 
 	private String extension;
+
+	private Integer likes;
+
+	private Integer views;
+
+	public static Thumbnail of(
+		MultipartFile file,
+		Restaurant restaurant,
+		Category category,
+		User user
+	) throws IOException {
+		BufferedImage bufferedImage = FileUtil.toBufferedImage(file);
+		long size = file.getSize();
+		int height = FileUtil.getHeight(bufferedImage);
+		int width = FileUtil.getWidth(bufferedImage);
+		String extension = FileUtil.getExtension(file);
+		String originalName = FileUtil.getOriginalName(file);
+		String originFileName = FileUtil.getOriginFileName(file);
+
+		return Thumbnail.builder()
+			.restaurant(restaurant)
+			.category(category)
+			.user(user)
+			.size(size)
+			.width(width)
+			.height(height)
+			.extension(extension)
+			.originalFileName(originalName)
+			.originalFileName(originFileName)
+			.views(0)
+			.likes(0)
+			.build();
+	}
 
 }
