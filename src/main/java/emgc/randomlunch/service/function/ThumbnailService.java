f19c3a2b344e10a076cd209456b574484635d05b
Limheonjun <jumong40@naver.com>
2021-12-10 21:13:40 +0900

    feat: createThumbnailWithHashtag메소드 추가

--- src/main/java/emgc/randomlunch/service/function/ThumbnailService.java
+++ src/main/java/emgc/randomlunch/service/function/ThumbnailService.java
@@ -1,9 +1,12 @@
 package emgc.randomlunch.service.function;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
 
+import emgc.randomlunch.dto.thumbnail.ThumbnailCreateRequest;
 import emgc.randomlunch.dto.thumbnail.ThumbnailResponse;
 
 public interface ThumbnailService {
@@ -29,5 +32,11 @@ public interface ThumbnailService {
 		String keyword,
 		Pageable pageable
 	);
+
+	ThumbnailResponse createThumbnailWithHashtag(
+		MultipartFile file,
+		String email,
+		ThumbnailCreateRequest request
+	) throws IOException;
 
 }
--- src/main/java/emgc/randomlunch/service/implementation/DefaultThumbnailService.java
+++ src/main/java/emgc/randomlunch/service/implementation/DefaultThumbnailService.java
@@ -1,22 +1,28 @@
 package emgc.randomlunch.service.implementation;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import emgc.randomlunch.dto.thumbnail.ThumbnailCreateRequest;
 import emgc.randomlunch.dto.thumbnail.ThumbnailResponse;
 import emgc.randomlunch.entity.Category;
 import emgc.randomlunch.entity.Hashtag;
 import emgc.randomlunch.entity.Restaurant;
 import emgc.randomlunch.entity.Thumbnail;
 import emgc.randomlunch.entity.ThumbnailHashtag;
+import emgc.randomlunch.entity.User;
 import emgc.randomlunch.repository.CategoryRepository;
 import emgc.randomlunch.repository.HashtagRepository;
 import emgc.randomlunch.repository.RestaurantRepository;
 import emgc.randomlunch.repository.ThumbnailHashtagRepository;
 import emgc.randomlunch.repository.ThumbnailRepository;
+import emgc.randomlunch.repository.UserRepository;
+import emgc.randomlunch.service.function.HashtagService;
 import emgc.randomlunch.service.function.ThumbnailService;
 import lombok.RequiredArgsConstructor;
 
@@ -33,6 +39,10 @@ public class DefaultThumbnailService implements ThumbnailService {
 	private final ThumbnailHashtagRepository thumbnailHashtagRepository;
 
 	private final CategoryRepository categoryRepository;
+
+	private final UserRepository userRepository;
+
+	private final HashtagService hashtagService;
 
 	@Override
 	public List<ThumbnailResponse> getThumbnailListByRestaurantName(
@@ -96,5 +106,31 @@ public class DefaultThumbnailService implements ThumbnailService {
 		);
 
 		return thumbnailList.stream().map(ThumbnailResponse::from).collect(Collectors.toList());
+	}
+
+	@Override
+	public ThumbnailResponse createThumbnailWithHashtag(
+		MultipartFile file,
+		String email,
+		ThumbnailCreateRequest request
+	) throws IOException {
+		Restaurant restaurant = restaurantRepository.findById(request.getRestaurantId())
+			.orElseThrow();//TODO : 구체적인 예외 처리
+		Category category = categoryRepository.findById(request.getCategoryId()).orElseThrow();
+		User user = userRepository.findByEmail(email).orElseThrow();
+		//해시태그가 있으면 가져오고 없으면 생성하고 저장
+		List<Hashtag> hashtagList = hashtagService.createHashtagList(request.getHashtagList());
+
+		//썸네일 생성
+		Thumbnail thumbnail = Thumbnail.of(file, restaurant, category, user);
+		Thumbnail savedThumbnail = thumbnailRepository.save(thumbnail);
+
+		//썸네일해시태그 생성
+		List<ThumbnailHashtag> thumbnailHashtagList = hashtagList.stream()
+			.map(hashtag -> ThumbnailHashtag.builder().hashtag(hashtag).thumbnail(savedThumbnail).build())
+			.collect(Collectors.toList());
+		thumbnailHashtagRepository.saveAll(thumbnailHashtagList);
+
+		return ThumbnailResponse.from(thumbnail);
 	}
 }
