975a9cc9c0d20289c0f35f65ab1d05e9b2bc8fca
Limheonjun <jumong40@naver.com>
2021-12-10 18:42:00 +0900

    refactor: 컨트롤러 유효성 검사 예외 핸들링 추가

--- src/main/java/emgc/randomlunch/controlleradvice/GlobalControllerAdvice.java
+++ src/main/java/emgc/randomlunch/controlleradvice/GlobalControllerAdvice.java
@@ -1,52 +1,29 @@
 package emgc.randomlunch.controlleradvice;
 
-import emgc.randomlunch.exception.NoSuchUserException;
-import lombok.extern.slf4j.Slf4j;
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.servlet.http.HttpServletRequest;
-import java.security.Principal;
+import emgc.randomlunch.dto.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalControllerAdvice {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity methodValidationException(
-            MethodArgumentNotValidException e,
-            HttpServletRequest request,
-            Principal principal
-    ){
-        String user = principal == null ? "anonymous" : principal.getName();
-        String message = e.getAllErrors().get(0).getDefaultMessage();
-        log.info("MethodArgumentNotValidException occured | user : {} , url : {} , trace : {}", user, request.getRequestURI(), e.getStackTrace());
-        log.info("Error Message : {}", message);
-        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+        MethodArgumentNotValidException e,
+        HttpServletRequest request
+    ) {
+        ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST, request.getRequestURI(),	e.getBindingResult());
+        return new ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(NoSuchUserException.class)
-    public ResponseEntity methodValidationException(
-            NoSuchUserException e,
-            HttpServletRequest request
-    ){
-        String message = e.getMessage();
-        log.info("NoSuchUserException occured | url : {} , trace : {}", request.getRequestURI(), e.getStackTrace());
-        log.info("Error Message : {}", message);
-        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity illegalArgumentException(
-            IllegalArgumentException e,
-            HttpServletRequest request
-    ){
-        String message = e.getMessage();
-        log.info("IllegalArgumentException occured | url : {} , trace : {}", request.getRequestURI(), e.getStackTrace());
-        log.info("Error Message : {}", message);
-        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
-    }
 }
