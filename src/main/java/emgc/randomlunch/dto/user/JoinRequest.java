3ebfb214c0eda3ea6e0ca23300b826618a160bb8
Limheonjun <jumong40@naver.com>
2021-12-10 19:15:27 +0900

    refactor: JoinRequest에 nickname속성 추가

--- src/main/java/emgc/randomlunch/dto/user/JoinRequest.java
+++ src/main/java/emgc/randomlunch/dto/user/JoinRequest.java
@@ -20,4 +20,8 @@ public class JoinRequest {
 	@Size(min = 8, max = 20, message = "비밀번호는 8~20자 사이여야 합니다.")
 	private String password;
 
+	@NotBlank(message = "닉네임은 필수 입력 값입니다.")
+	@Size(min = 1, max = 20, message = "닉네임은 1~20자 사이여야 합니다.")
+	private String nickname;
+
 }
