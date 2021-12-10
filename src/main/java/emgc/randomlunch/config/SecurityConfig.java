84ff191fa4b2d205f37cbb14ade49d2b08fed952
Limheonjun <jumong40@naver.com>
2021-12-10 19:31:02 +0900

    refactor: PasswordEncoder를 BCryptPasswordEncoder로 교체

--- src/main/java/emgc/randomlunch/config/SecurityConfig.java
+++ src/main/java/emgc/randomlunch/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -34,14 +35,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	}
 
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/image/**", "/docs/**");
-	}
-
 	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
