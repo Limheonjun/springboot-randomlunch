9f5abe9c38048734760c0bae51036723ecaee634
Limheonjun <jumong40@naver.com>
2021-12-10 19:49:18 +0900

    refactor: 예외 처리 메소드 추가
    - HttpRequestMethodNotSupportedException
    - Exception

--- src/main/java/emgc/randomlunch/controlleradvice/GlobalControllerAdvice.java
+++ src/main/java/emgc/randomlunch/controlleradvice/GlobalControllerAdvice.java
@@ -1,11 +1,10 @@
 package emgc.randomlunch.controlleradvice;
 
-import java.security.Principal;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -22,8 +21,29 @@ public class GlobalControllerAdvice {
         MethodArgumentNotValidException e,
         HttpServletRequest request
     ) {
-        ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST, request.getRequestURI(),	e.getBindingResult());
+        log.error("handleMethodArgumentNotValidException", e);
+        final ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST, request.getRequestURI(), e.getBindingResult());
         return new ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(
+        HttpRequestMethodNotSupportedException e,
+        HttpServletRequest request
+    ) {
+        log.error("handleHttpRequestMethodNotSupportedException", e);
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.METHOD_NOT_ALLOWED, request.getRequestURI());
+        return new ResponseEntity<>(response, HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(
+        Exception e,
+        HttpServletRequest request
+    ) {
+        log.error("handleEntityNotFoundException", e);
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR, request.getRequestURI()   );
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
