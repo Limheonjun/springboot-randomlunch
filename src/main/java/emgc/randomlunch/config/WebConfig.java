a075ad904d878003cdac3a3030a9815728c8cfa6
Limheonjun <jumong40@naver.com>
2021-12-10 19:50:06 +0900

    refactor: 인터셉터 제거

--- src/main/java/emgc/randomlunch/config/WebConfig.java
+++ src/main/java/emgc/randomlunch/config/WebConfig.java
@@ -1,11 +1,9 @@
 package emgc.randomlunch.config;
 
-import emgc.randomlunch.interceptor.UrlPathInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,11 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Value("${file.thumbnail.path}")
     private String path;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new UrlPathInterceptor());
-    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
