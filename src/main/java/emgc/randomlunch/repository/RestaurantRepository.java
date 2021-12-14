b56c563546ec58a7298cd483b7134dd022d4fbf3
o,hepmkim <jumong40@naver.com>
2021-12-15 08:19:29 +0900

    feat: 반경 내 음식점 검색 쿼리 메소드 추가

--- src/main/java/emgc/randomlunch/repository/RestaurantRepository.java
+++ src/main/java/emgc/randomlunch/repository/RestaurantRepository.java
@@ -1,13 +1,32 @@
 package emgc.randomlunch.repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import emgc.randomlunch.entity.Restaurant;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
 	List<Restaurant> findAllByNameLike(String name);
+
+	@Query(value = "SELECT *, "
+		+ "(6371*acos(cos(radians(:latitude))*cos(radians(latitude))*cos(radians(longitude)"
+		+ "-radians(:longitude))+sin(radians(:latitude))*sin(radians(latitude)))) "
+		+ "AS distance "
+		+ "FROM restaurant "
+		+ "HAVING distance <= :distance  "
+		+ "ORDER BY distance "
+		+ "LIMIT :offset, :limit", nativeQuery = true)
+	List<Restaurant> findAllByGps(
+		@Param(value = "latitude") BigDecimal latitude,
+		@Param(value = "longitude") BigDecimal longitude,
+		@Param(value = "distance") int distance,
+		@Param(value = "offset") int offset,
+		@Param(value = "limit") int limit
+	);
 
 }
