70e2d6acd12f2d20db97a160b2660c0486ea1cfe
o,hepmkim <jumong40@naver.com>
2021-12-17 17:59:11 +0900

    feat: 카테고리 추가

---
+++ src/main/java/emgc/randomlunch/enums/Category.java
@@ -0,0 +1,29 @@
+package emgc.randomlunch.enums;
+
+public enum Category {
+	CHICKEN("치킨"),
+	DESSERT("카페/디저트"),
+	SNACK("분식"),
+	PIZZA("피자"),
+	ASIAN("아시안"),
+	CHINESE("중국집"),
+	PORK_FEET("족발/보쌈"),
+	STEAMED_SOUP("찜/탕"),
+	BURGER("버거"),
+	MEAT("고기"),
+	KOREAN("한식"),
+	WESTERN("양식"),
+	SUSHI("회/초밥"),
+	ETC("기타")
+	;
+
+	private String value;
+
+	Category(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return this.value;
+	}
+}
