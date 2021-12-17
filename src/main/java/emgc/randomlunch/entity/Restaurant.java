311293ea01be0d9cb85f7e09407c0e0fecfbf05b
o,hepmkim <jumong40@naver.com>
2021-12-17 20:32:02 +0900

    refactor: 음식점 속성 수정 및 추가

--- src/main/java/emgc/randomlunch/entity/Restaurant.java
+++ src/main/java/emgc/randomlunch/entity/Restaurant.java
@@ -1,15 +1,19 @@
 package emgc.randomlunch.entity;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import emgc.randomlunch.enums.RestaurantStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -34,10 +38,19 @@ public class Restaurant extends BaseEntity {
 
 	private String name;
 
-	private String address;
+	private String lotNumberAddress;
 
-	private String latitude;
+	private String streetNameAddress;
 
-	private String longitude;
+	@Enumerated(EnumType.STRING)
+	private RestaurantStatus status;
+
+	private Double x;
+
+	private Double y;
+
+	private BigDecimal latitude;
+
+	private BigDecimal longitude;
 
 }
