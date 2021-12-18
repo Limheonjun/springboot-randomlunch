a4f79b535bbe80955c659b9d9903a49d1671bcaf
o,hepmkim <jumong40@naver.com>
2021-12-18 16:05:06 +0900

    refactor: gps와 반경으로 음식점 찾는 메소드명 수정

--- src/main/java/emgc/randomlunch/repository/RestaurantRepository.java
+++ src/main/java/emgc/randomlunch/repository/RestaurantRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import emgc.randomlunch.entity.Restaurant;
+import emgc.randomlunch.enums.RestaurantStatus;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
@@ -18,15 +19,18 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 		+ "-radians(:longitude))+sin(radians(:latitude))*sin(radians(latitude)))) "
 		+ "AS distance "
 		+ "FROM restaurant "
-		+ "HAVING distance <= :distance  "
+		+ "WHERE status = :status AND name LIKE CONCAT('%', :name, '%') "
+		+ "HAVING distance <= :distance  " //0.1 = 1km, 0.01 = 100m
 		+ "ORDER BY distance "
-		+ "LIMIT :offset, :limit", nativeQuery = true)
-	List<Restaurant> findAllByGps(
+		+ "LIMIT :offset, :limit ", nativeQuery = true)
+	List<Restaurant> findAllByNameAndGPSAndDistance(
 		@Param(value = "latitude") BigDecimal latitude,
 		@Param(value = "longitude") BigDecimal longitude,
-		@Param(value = "distance") int distance,
-		@Param(value = "offset") int offset,
-		@Param(value = "limit") int limit
+		@Param(value = "distance") Float distance,
+		@Param(value = "offset") Integer offset,
+		@Param(value = "limit") Integer limit,
+		@Param(value = "status") String status,
+		@Param(value = "name") String name
 	);
 
 }
