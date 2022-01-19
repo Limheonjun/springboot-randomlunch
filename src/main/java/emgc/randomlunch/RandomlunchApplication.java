af582266e65561b1bc9d0072a25b4798733e7575
o,hepmkim <jumong40@naver.com>
2022-01-19 17:14:47 +0900

    feat: 서버 가동 시 aws.yml 설정 파일 읽도록 설정

--- src/main/java/emgc/randomlunch/RandomlunchApplication.java
+++ src/main/java/emgc/randomlunch/RandomlunchApplication.java
@@ -1,7 +1,8 @@
 package emgc.randomlunch;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
@@ -9,8 +10,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class RandomlunchApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+        + "classpath:application.yml,"
+        + "classpath:aws.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(RandomlunchApplication.class, args);
+        new SpringApplicationBuilder(RandomlunchApplication.class)
+            .properties(APPLICATION_LOCATIONS)
+            .run(args);
     }
 
 }
